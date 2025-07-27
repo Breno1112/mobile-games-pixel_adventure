f000bd4f59094f27b9b8d66f1c2926e47b1a9961
Breno1112 <contato.brenofachini@hotmail.com>
2025-07-27 05:18:58 -0300

    animations done. Need to work with collision detection

--- lib/entities/player/player_factory.dart
+++ lib/entities/player/player_factory.dart
@@ -47,18 +47,18 @@ class Player extends SpriteAnimationGroupComponent<PlayerState> with HasGameRefe
   Future<void> onLoad() async {
 
     animations = <PlayerState, SpriteAnimation>{
-      PlayerState.doubleJump: await createAnimation('main_characters/$spriteBaseName/Double Jump (32x32).png', 11, 0.1, Vector2(32, 32)),
-      PlayerState.fall: await createAnimation('main_characters/$spriteBaseName/Fall (32x32).png', 11, 0.1, Vector2(32, 32)),
-      PlayerState.hit: await createAnimation('main_characters/$spriteBaseName/Hit (32x32).png', 11, 0.1, Vector2(32, 32)),
+      PlayerState.doubleJump: await createAnimation('main_characters/$spriteBaseName/Double Jump (32x32).png', 6, 0.1, Vector2(32, 32), loop: false),
+      PlayerState.fall: await createAnimation('main_characters/$spriteBaseName/Fall (32x32).png', 1, 0.1, Vector2(32, 32), loop: false),
+      PlayerState.hit: await createAnimation('main_characters/$spriteBaseName/Hit (32x32).png', 7, 0.1, Vector2(32, 32), loop: false),
       PlayerState.idle: await createAnimation('main_characters/$spriteBaseName/Idle (32x32).png', 11, 0.1, Vector2(32, 32)),
-      PlayerState.jump: await createAnimation('main_characters/$spriteBaseName/Jump (32x32).png', 11, 0.1, Vector2(32, 32)),
-      PlayerState.run: await createAnimation('main_characters/$spriteBaseName/Run (32x32).png', 11, 0.1, Vector2(32, 32)),
-      PlayerState.wallJump: await createAnimation('main_characters/$spriteBaseName/Wall Jump (32x32).png', 11, 0.1, Vector2(32, 32)),
+      PlayerState.jump: await createAnimation('main_characters/$spriteBaseName/Jump (32x32).png', 1, 0.1, Vector2(32, 32), loop: false),
+      PlayerState.run: await createAnimation('main_characters/$spriteBaseName/Run (32x32).png', 12, 0.05, Vector2(32, 32)),
+      PlayerState.wallJump: await createAnimation('main_characters/$spriteBaseName/Wall Jump (32x32).png', 5, 0.1, Vector2(32, 32), loop: false),
     };
     current = PlayerState.idle;
   }
 
-  Future<SpriteAnimation> createAnimation(String path, int count, double stepTime, Vector2 size) async {
+  Future<SpriteAnimation> createAnimation(String path, int count, double stepTime, Vector2 size, {bool loop = true}) async {
     final image = await game.images.load(path);
     SpriteAnimation animation = SpriteAnimation.fromFrameData(
       image,
@@ -66,6 +66,7 @@ class Player extends SpriteAnimationGroupComponent<PlayerState> with HasGameRefe
         amount: count,          // number of frames
         stepTime: stepTime,       // duration per frame (in seconds)
         textureSize: size, // size of each frame
+        loop: loop
       ),
     );
     return animation;
